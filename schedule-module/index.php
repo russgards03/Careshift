a0be0b2d6f3ca3efde2f86f03cebfa18f9c1e8c6
Russ Allen Garde <99627318+russgards03@users.noreply.github.com>
2024-10-13 12:00:45 +0800

    Add files via upload

--- schedule-module/index.php
+++ schedule-module/index.php
@@ -1,39 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>Schedule</title>
-</head>
-<body>
-    <h1>Schedule</h1>
-    <div class="dashboard-wrapper">
-        <form method="POST" action="">
-            <div class="button-container">
-                <button type="submit" name="page" value="add-schedule">Add Schedule</button>
-                <button type="submit" name="page" value="edit-schedule">Edit Schedule</button>
-                <button type="submit" name="page" value="calendar">Calendar</button>
-            </div>
-        </form>
-        <div class="main_content">
-            <?php
-                $page = isset($_POST['page']) ? $_POST['page'] : 'calendar';
-
-                switch($page){
-                    case 'add-schedule':
-                        require_once 'schedule-module/add-sched.php';
-                        break;
-                    case 'edit-schedule':
-                        require_once 'schedule-module/edit-sched.php';
-                        break;
-                    case 'calendar':
-                    default:
-                        require_once 'schedule-module/calendar.php';
-                        break; 
-                }
-            ?>
-        </div>
+<div class="content_wrapper">
+    <div class="heading">
+        <h1><i class="fas fa-solid fa-clock"></i>&nbspSchedule</h1>
+        <a href="index.php?page=schedule&subpage=add-sched" class="right_button"><i class="fa fa-plus"></i>&nbspAdd Schedule</a>
+        <a href="index.php?page=schedule&subpage=calendar" class="right_button"><i class="fa fa-calendar"></i>&nbspCalendar</a>
     </div>
-</body>
-</html>
+</div>
+<?php
+    $subpage = isset($_GET['subpage']) ? $_GET['subpage'] : 'calendar';
+
+    switch($subpage){
+        case 'add-sched':
+            require_once 'add_schedule.php';
+        break;
+        case 'calendar':
+            require_once 'calendar.php';
+        break; 
+        default:
+            require_once 'calendar.php';
+        break;
+    }
+?>
